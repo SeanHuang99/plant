975a1886945f951ab70470514587e5e89ebe5e51
gy <2581677302@qq.com>
2024-05-08 11:01:16 +0100

    add .gitignore

---
+++ public/javascript/detail.js
@@ -0,0 +1,23 @@
+window.onload=function (){
+    const plantId=localStorage.getItem('plantId')
+    console.log('plantId: '+plantId)
+    if (plantId==null){
+        console.log('plantId==null')
+        //return to main page, and show alert of 'cannot find plant'
+    }
+    console.log('lalala')
+    openPlantsIDB().then(IDB=>{
+        getDetailById(IDB,plantId).then(plant=>{
+            console.log('plant found in IDB ----- '+typeof plant)
+            console.log(plant.plantId+'-------> '+plant.description)
+        }).catch(err=> {
+            console.log(err)
+        })
+        // if (plant!=null){
+        //   //todo:insert html code
+        // }
+        // else {//return to main page, and show alert of 'cannot find plant'
+        //   console.log('plant==null')
+        // }
+    })
+}

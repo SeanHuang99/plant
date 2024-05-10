104539413695da0cca535af5f55a430e84801f74
czc <870667475@qq.com>
2024-05-10 22:26:21 +0100

    update

--- public/javascript/detail.js
+++ public/javascript/detail.js
@@ -1,65 +1,110 @@
 window.onload=function (){
-    window.localStorage.setItem('lastURL','/detail');
+    if(window.localStorage.getItem("lastURL")!=='/detail' || window.localStorage.getItem("lastURL")===undefined) {
+        window.localStorage.setItem('lastURL', '/detail');
+    }
+    // synPlantFromServer();
     // console.log("wel to detail");
+    generateDetailPage();
+}
+
+function generateDetailPage(){
     const plantId=localStorage.getItem('plantId')
-    console.log('plantId: '+plantId)
+    // console.log('plantId: '+plantId)
     if (plantId==null){
         console.log('plantId==null')
         //todo: return to main page, and show alert of 'cannot find plant'
     }
-    openPlantsIDB().then(IDB=>{
-        getDetailById(IDB,plantId).then(plant=>{
-            console.log('plant found in IDB ----- '+JSON.stringify(plant))
-            console.log(plant.plantId+'-------> '+plant.description)
+    openPlantsIDB().then(IDB=> {
+        getDetailById(IDB, plantId).then(plant => {
+            console.log('plant found in IDB ----- ' + JSON.stringify(plant))
+            console.log(plant.plantId + '-------> ' + plant.description)
             detailRender(plant)
-        }).catch(err=> {
+        }).catch(err => {
             console.log(err)
             //todo: return to main page, and show alert of 'cannot find plant'
         })
     })
+}
+
 
     //render the page manully
-    function detailRender(plant){
-        document.getElementById('plantName').textContent = `Plant Name: ${plant.plantName}`;
-        document.getElementById('plantDescription').textContent = `Description: ${plant.description}`;
-        document.getElementById('plantDetail').textContent = `Detail: ${plant.details}`;
-        document.getElementById('plantDate').textContent = `Date/Time: ${plant.datetime}`;
-        document.getElementById('flowerColorBox').style.backgroundColor = `${plant.flowerColor}`;
-        document.getElementById('flowerColor').textContent = `${plant.flowerColor}`;
-        document.getElementById('nickName').textContent = `Nick Name: ${plant.nickName}`;
-        document.getElementById('flowers').textContent = `Flowers: ${plant.flowers}`;
-        document.getElementById('sunExposure').textContent = `Sun Exposure: ${plant.sunExposure}`;
-        document.getElementById('status').textContent = `Status: ${plant.status}`;
+function detailRender(plant){
+    document.getElementById('plantName').textContent = `Plant Name: ${plant.plantName}`;
+    document.getElementById('plantDescription').textContent = `Description: ${plant.description}`;
+    document.getElementById('plantDetail').textContent = `Detail: ${plant.details}`;
+    document.getElementById('plantDate').textContent = `Date/Time: ${plant.datetime}`;
+    document.getElementById('flowerColorBox').style.backgroundColor = `${plant.flowerColor}`;
+    document.getElementById('flowerColor').textContent = `${plant.flowerColor}`;
+    document.getElementById('nickName').textContent = `Nick Name: ${plant.nickName}`;
+    document.getElementById('flowers').textContent = `Flowers: ${plant.flowers}`;
+    document.getElementById('sunExposure').textContent = `Sun Exposure: ${plant.sunExposure}`;
+    document.getElementById('status').textContent = `Status: ${plant.status}`;
 
-        document.getElementById('photo').src = plant.photo;
-        document.getElementById('who_you_are').textContent = `You are in room: ${plant.plantId}`;
+    document.getElementById('photo').src = plant.photo;
+    document.getElementById('who_you_are').textContent = `You are in room: ${plant.plantId}`;
 
-        if(plant.dbpedia.link!==undefined || plant.dbpedia.link!==""){
-            document.getElementById('DBpediaLink').href = `${plant.dbpedia.link}`;
-        }
-        else{
-            document.getElementById('DBpediaLink').href = 'http://dbpedia.org/resource/';
-        }
-
-        if(plant.dbpedia.name!==undefined || plant.dbpedia.name!==""){
-            document.getElementById('DBpediaName').textContent = `Common Name: ${plant.dbpedia.name}`;
-        }
-        else{
-            document.getElementById('DBpediaName').textContent = `Common Name: None`;
-        }
-
-        if(plant.dbpedia.description!==undefined || plant.dbpedia.description!==""){
-            document.getElementById('DBpediaDescription').textContent = `Plant Description: ${plant.dbpedia.description}`;
-        }
-        else{
-            document.getElementById('DBpediaDescription').textContent = `Plant Description: None`;
-        }
-
-        if(plant.dbpedia.genus!==undefined || plant.dbpedia.genus!==""){
-            document.getElementById('DBpediaGunes').textContent = `Plant Genus: ${plant.dbpedia.genus}`;
-        }
-        else{
-            document.getElementById('DBpediaGunes').textContent = `Plant Genus: None`;
-        }
+    if(plant.dbpedia.link!==undefined || plant.dbpedia.link!==""){
+        document.getElementById('DBpediaLink').href = `${plant.dbpedia.link}`;
     }
+    else{
+        document.getElementById('DBpediaLink').href = 'http://dbpedia.org/resource/';
+    }
+
+    if(plant.dbpedia.name!==undefined || plant.dbpedia.name!==""){
+        document.getElementById('DBpediaName').textContent = `Common Name: ${plant.dbpedia.name}`;
+    }
+    else{
+        document.getElementById('DBpediaName').textContent = `Common Name: None`;
+    }
+
+    if(plant.dbpedia.description!==undefined || plant.dbpedia.description!==""){
+        document.getElementById('DBpediaDescription').textContent = `Plant Description: ${plant.dbpedia.description}`;
+    }
+    else{
+        document.getElementById('DBpediaDescription').textContent = `Plant Description: None`;
+    }
+
+    if(plant.dbpedia.genus!==undefined || plant.dbpedia.genus!==""){
+        document.getElementById('DBpediaGunes').textContent = `Plant Genus: ${plant.dbpedia.genus}`;
+    }
+    else{
+        document.getElementById('DBpediaGunes').textContent = `Plant Genus: None`;
+    }
+}
+
+
+function openEditPopup() {
+    document.getElementById('editPopup').style.display = 'flex';
+}
+
+function closeEditPopup() {
+    document.getElementById('editPopup').style.display = 'none';
+}
+
+
+// Function to submit a request to update the plant name
+async function submitRequest() {
+    const plantId = getpPlantId();
+    const nickName = getNickName();
+    const preferredPlantName = document.getElementById('preferredPlantName').value;
+
+    // Post the data to the backend via the "/updatePlants" route
+    const response = await fetch('/requestHandler/updatePlantsRequest', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ plantId, preferredPlantName, nickName })
+    });
+
+    if (response.ok) {
+        synPlantFromServer();
+        generateDetailPage();
+        alert('Request submitted successfully!');
+    } else {
+        alert('Error submitting request');
+    }
+    // Close the modal after submitting
+    // $('#editModal').modal('hide');
+    closeEditPopup()
 }
--- public/javascript/idb-utility.js
+++ public/javascript/idb-utility.js
@@ -18,24 +18,24 @@ function synPlantFromServer() {
                 openPlantsIDB().then((db) => {
                     getAllPlants(db).then(plants => {
                         //如果是第一次连接（IDB没有数据：长度=0）
-                        if (plants.length === 0) {
-                            //添加所有plant
-                            addNewPlantsToIDB(db, newPlants).then(() => {
-                                console.log("All new plants added to IDB")
-                            })
-                        }
+                        // if (plants.length === 0) {
+                        //添加所有plant
+                        addNewPlantsToIDB(db, newPlants).then(() => {
+                            console.log("All new plants added to IDB")
+                        })
+                        // }
                         //如果不是，判断plant的长度是否相同
-                        else {//相同 则正确
-                            //否则重新添加
-                            if (plants.length !== newPlants.length) {
-                                deleteAllExistingPlantsFromIDB(db).then(() => {
-                                    console.log('deleteAllExistingPlantsFromIDB')
-                                    addNewPlantsToIDB(db, newPlants).then(() => {
-                                        console.log("All new plants added to IDB")
-                                    })
-                                })
-                            }
-                        }
+                        // else {//相同 则正确
+                        //     //否则重新添加
+                        //     if (plants.length !== newPlants.length) {
+                        //         deleteAllExistingPlantsFromIDB(db).then(() => {
+                        //             console.log('deleteAllExistingPlantsFromIDB')
+                        //             addNewPlantsToIDB(db, newPlants).then(() => {
+                        //                 console.log("All new plants added to IDB")
+                        //             })
+                        //         })
+                        //     }
+                        // }
                     })
                 })
             })

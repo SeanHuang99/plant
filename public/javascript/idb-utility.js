98e2a0411e5daaed4592d9ba8f3b8efbe7bfd274
czc <870667475@qq.com>
2024-05-10 21:23:07 +0100

    update plantname change problem

--- public/javascript/idb-utility.js
+++ public/javascript/idb-utility.js
@@ -1,3 +1,53 @@
+
+function synDataFromServer() {
+    if (navigator.onLine) {
+        // syn plant information indexed db
+        synPlantFromServer();
+        synChatRecordFromServer();
+    }
+}
+
+
+function synPlantFromServer() {
+    if (navigator.onLine) {
+        fetch('http://localhost:3000/requestHandler/getAllPlants')
+            .then(function (res) {
+                return res.json();
+            })
+            .then(function (newPlants) {
+                openPlantsIDB().then((db) => {
+                    getAllPlants(db).then(plants => {
+                        //如果是第一次连接（IDB没有数据：长度=0）
+                        if (plants.length === 0) {
+                            //添加所有plant
+                            addNewPlantsToIDB(db, newPlants).then(() => {
+                                console.log("All new plants added to IDB")
+                            })
+                        }
+                        //如果不是，判断plant的长度是否相同
+                        else {//相同 则正确
+                            //否则重新添加
+                            if (plants.length !== newPlants.length) {
+                                deleteAllExistingPlantsFromIDB(db).then(() => {
+                                    console.log('deleteAllExistingPlantsFromIDB')
+                                    addNewPlantsToIDB(db, newPlants).then(() => {
+                                        console.log("All new plants added to IDB")
+                                    })
+                                })
+                            }
+                        }
+                    })
+                })
+            })
+    }
+}
+
+
+function synChatRecordFromServer(){
+
+}
+
+
 function createPlantId(plantName) {
     const now = new Date();
     const year = now.getFullYear();
--- public/javascript/main.js
+++ public/javascript/main.js
@@ -21,30 +21,7 @@ if (navigator.onLine) {
             return res.json();
         })
         .then(function (newPlants) {
-            openPlantsIDB().then((db) => {
-                getAllPlants(db).then(plants => {
-                    //如果是第一次连接（IDB没有数据：长度=0）
-                    if (plants.length === 0) {
-                        //添加所有plant
-                        addNewPlantsToIDB(db, newPlants).then(() => {
-                            console.log("All new plants added to IDB")
-                        })
-                    }
-                    //如果不是，判断plant的长度是否相同
-                    else {//相同 则正确
-                        //否则重新添加
-                        if (plants.length !== newPlants.length) {
-                            deleteAllExistingPlantsFromIDB(db).then(() => {
-                                console.log('deleteAllExistingPlantsFromIDB')
-                                addNewPlantsToIDB(db, newPlants).then(() => {
-                                    console.log("All new plants added to IDB")
-                                })
-                            });
-                        }
-                    }
-                })
-            });
-            // return newPlants
+            synPlantFromServer();
             renderPlantList(newPlants)
         })
 }

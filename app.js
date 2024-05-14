39d45b1623d99e06f6187a6d2e01e9d41ce0de4d
Tommy <870667475@qq.com>
2024-05-14 02:42:01 +0100

    change app.js

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.use(logger('dev'));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.json({ limit: '100mb' }));
-app.use(express.urlencoded({ extended: true, limit: '100mb' }));
+app.use(express.json({ limit: '40mb' }));
+app.use(express.urlencoded({ extended: true, limit: '40mb' }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
